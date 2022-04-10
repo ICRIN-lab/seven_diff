1a46c8ac1adf654d2272eefc5956926f54e81719
mselleki <mselleki98@gmail.com>
2022-04-10 23:25:43 +0200

    modify instructions

--- seven_diff.py
+++ seven_diff.py
@@ -39,7 +39,7 @@ class SevenDiff(TaskTemplate):
 
     instructions = [
         f"Dans cette tâche cognitive, il vous est demandé de repérer les différences entre deux images. \n\n Appuyez sur la touche '{yes_key_name}' pour répondre oui ou pour selectionner la réponse "
-        f"de droite. \n\n Appuyez sur la touche '{no_key_name}' pour répondre non ou pour selectionner la réponse de "
+        f"de droite. Appuyez sur la touche '{no_key_name}' pour répondre non ou pour selectionner la réponse de "
         f"gauche.", "N'appuyez sur les touches que lorsqu'on vous le demande.",
         f"Placez vos index sur les touches '{no_key_name}' et '{yes_key_name}.'"]
 
