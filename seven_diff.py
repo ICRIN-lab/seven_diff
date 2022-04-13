b749cfffec07c9254047b0679eae45a311cb3f1e
mselleki <mselleki98@gmail.com>
2022-04-14 01:43:19 +0200

    minor fix

--- seven_diff.py
+++ seven_diff.py
@@ -14,13 +14,13 @@ class SevenDiff(TaskTemplate):
     yes_key_code = "p"
     keys = ["space", left_key, mid_left_key, right_key, mid_right_key, quit_code]
 
-    next = f"Pour passer à l'instruction suivante, appuyez sur la touche {right_key}"
+    next = f"Pour passer à l'instruction suivante, appuyez sur la touche {yes_key_code}"
 
     instructions = [
-        f"Dans cette tâche cognitive, il vous est demandé de repérer les différences entre deux images. \n\n Appuyez "
+        f"Dans cette expérience, il vous est demandé de repérer les différences entre deux images. \n\n Appuyez "
         f"sur la touche '{right_key}' pour répondre oui ou pour selectionner la réponse "
         f"de droite. \n\n Appuyez sur la touche '{left_key}' pour répondre non ou pour selectionner la réponse de "
-        f"gauche.", f"Placez vos index sur les touches '{left_key}' et '{right_key}.'"]
+        f"gauche.", f"Placez vos index sur les touches '{left_key}' et '{right_key}.'"]  # à reformuler
     font_size_instr = 0.05
 
     csv_headers = ['id_candidate', 'no_trial', 'nb_diff', 'ans_candidate',
@@ -32,7 +32,7 @@ class SevenDiff(TaskTemplate):
         self.win.flip()
         core.wait([.1 if no_trial <= 50 else .5][0])
         self.create_visual_text(
-                "Combien voyez-vous de différences entre ces deux images ?").draw()
+            "Combien voyez-vous de différences entre ces deux images ?").draw()
         self.create_visual_text("0 \t\t/\t\t 1", pos=(-.6, -.4)).draw()
         self.create_visual_text("2 \t\t/\t\t 3+", pos=(.6, -.4)).draw()
         self.win.flip()
@@ -40,14 +40,18 @@ class SevenDiff(TaskTemplate):
         good_ans = self.get_good_ans(images[0][-5], {"0": self.left_key,
                                                      "1": self.mid_left_key,
                                                      "2": self.mid_right_key,
-                                                     "3": self.right_key})
+                                                     "3": self.right_key,
+                                                     "4": self.right_key,
+                                                     "5": self.right_key,
+                                                     "6": self.right_key,
+                                                     })
         if resp == good_ans:
             result = 1
         else:
             result = 0
         self.update_csv(self.participant, no_trial, images[0][-5], resp, good_ans, result,
                         round(rt, 2), round(time.time() - exp_start_timestamp, 2))
-        images.pop(0) # index out of range, il faut agrandir la liste_images
+        images.pop(0)  # index out of range, il faut agrandir la liste_images
         print(images)
         self.check_break(no_trial, 95, 259)
 
