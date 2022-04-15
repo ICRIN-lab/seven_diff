702714cac89758e8488ced00bce884f9c814e2b7
mselleki <mselleki98@gmail.com>
2022-04-15 16:20:07 +0200

    response pad version

--- seven_diff.py
+++ seven_diff.py
@@ -1,4 +1,7 @@
 import time
+
+import pyxid2 as pyxid2
+
 from list_images import images
 from psychopy import core
 from task_template import TaskTemplate
@@ -6,21 +9,25 @@ from task_template import TaskTemplate
 
 class SevenDiff(TaskTemplate):
     trials = 194
-    left_key = "a"
-    mid_left_key = "z"
-    mid_right_key = "o"
-    right_key = "p"
-    quit_code = "q"
-    yes_key_code = "p"
-    keys = ["space", left_key, mid_left_key, right_key, mid_right_key, quit_code]
+    left_key_name = "a"
+    left_key_code = "0"
+    mid_left_key_name = "z"
+    mid_left_key_code = "1"
+    mid_right_key_name = "o"
+    mid_right_key_code = "5"
+    right_key_name = "p"
+    right_key_code = "6"
+    quit_code = "3"
+    yes_key_code = "6"
+    keys = [left_key_code, mid_left_key_code, right_key_code, mid_right_key_code, yes_key_code, quit_code]
 
     next = f"Pour passer à l'instruction suivante, appuyez sur la touche {yes_key_code}"
 
     instructions = [
         f"Dans cette expérience, il vous est demandé de repérer les différences entre deux images. \n\n Appuyez "
-        f"sur la touche '{right_key}' pour répondre oui ou pour selectionner la réponse "
-        f"de droite. \n\n Appuyez sur la touche '{left_key}' pour répondre non ou pour selectionner la réponse de "
-        f"gauche.", f"Placez vos index sur les touches '{left_key}' et '{right_key}.'"]  # à reformuler
+        f"sur la touche '{right_key_code}' pour répondre oui ou pour selectionner la réponse "
+        f"de droite. \n\n Appuyez sur la touche '{left_key_code}' pour répondre non ou pour selectionner la réponse de "
+        f"gauche.", f"Placez vos index sur les touches '{left_key_code}' et '{right_key_code}.'"]  # à reformuler
     font_size_instr = 0.05
 
     csv_headers = ['id_candidate', 'no_trial', 'nb_diff', 'ans_candidate',
@@ -36,29 +43,31 @@ class SevenDiff(TaskTemplate):
         self.create_visual_image(image=f'img/{images[group][0]}',
                                  size=self.size(images[group][0])).draw()
         self.win.flip()
-        core.wait([8 if no_trial <= 94 else 6][0])
+        core.wait([.5 if no_trial <= 94 else .5][0])
         self.create_visual_text(
             "Combien voyez-vous de différences entre ces deux images ?").draw()
         self.create_visual_text("0 \t\t/\t\t 1", pos=(-.6, -.4)).draw()
         self.create_visual_text("2 \t\t/\t\t 3+", pos=(.6, -.4)).draw()
         self.win.flip()
-        resp, rt = self.get_response_with_time()
-        good_ans = self.get_good_ans(images[group][0][-5], {"0": self.left_key,
-                                                            "1": self.mid_left_key,
-                                                            "2": self.mid_right_key,
-                                                            "3": self.right_key,
-                                                            "4": self.right_key,
-                                                            "5": self.right_key,
-                                                            "6": self.right_key,
+        time_stamp = time.time() - exp_start_timestamp
+        resp, abs_time = self.get_response_with_time_response_pad(self.dev)
+        print(resp, abs_time)
+        good_ans = self.get_good_ans(images[group][0][-5], {"0": self.left_key_code,
+                                                            "1": self.mid_left_key_code,
+                                                            "2": self.mid_right_key_code,
+                                                            "3": self.right_key_code,
+                                                            "4": self.right_key_code,
+                                                            "5": self.right_key_code,
+                                                            "6": self.right_key_code,
                                                             })
         if resp == good_ans:
             result = 1
         else:
             result = 0
         self.update_csv(self.participant, no_trial, images[group][0][-5], resp, good_ans, result,
-                        round(rt, 2), round(time.time() - exp_start_timestamp, 2))
+                        round(abs_time - time_stamp, 2), round(abs_time, 2))
         images[group].pop(0)
-        self.check_break(no_trial, 94, 144)
+        self.check_break(no_trial, 94, 144, test=True)
 
 
 exp = SevenDiff(csv_folder="csv")
