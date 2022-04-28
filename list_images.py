21ddea60e47a7dadd8ff508c2837975fac870c70
mselleki <mselleki98@gmail.com>
2022-04-28 14:23:53 +0200

    final version seven diff

--- list_images.py
+++ list_images.py
@@ -1,5 +1,4 @@
 images = [
-<<<<<<< HEAD
     ['img_7_0.png', 'img_14_3.png', 'img_10_6.png', 'img_17_0.png', 'img_3_3.png', 'img_2_0.png', 'img_13_1.png',
      'img_19_4.png', 'img_13_0.png', 'img_8_3.png', 'img_11_2.png', 'img_15_1.png', 'img_0_5.png', 'img_5_3.png',
      'img_3_1.png', 'img_19_1.png', 'img_13_2.png', 'img_9_2.png', 'img_15_0.png', 'img_16_5.png', 'img_2_2.png',
@@ -24,40 +23,6 @@ images = [
      'img_26_4.png', 'img_22_2.png', 'img_23_3.png', 'img_27_6.png', 'img_21_0.png', 'img_28_5.png', 'img_24_3.png',
      'img_28_6.png', 'img_25_5.png', 'img_29_3.png', 'img_25_0.png', 'img_21_1.png', 'img_20_5.png', 'img_25_1.png',
      'img_27_4.png'],
-=======
-    ['img_9_1.png', 'img_17_5.png', 'img_11_3.png', 'img_13_1.png', 'img_13_0.png', 'img_11_2.png', 'img_9_0.png',
-     'img_15_4.png', 'img_9_2.png', 'img_11_0.png', 'img_13_2.png', 'img_11_1.png', 'img_9_3.png', 'img_15_1.png',
-     'img_17_3.png', 'img_11_5.png', 'img_17_2.png', 'img_15_0.png', 'img_15_2.png', 'img_17_0.png', 'img_9_4.png',
-     'img_13_4.png', 'img_13_5.png', 'img_17_1.png', 'img_15_3.png', 'img_10_0.png', 'img_8_2.png', 'img_16_6.png',
-     'img_14_4.png', 'img_8_3.png', 'img_10_1.png', 'img_12_3.png', 'img_12_1.png', 'img_8_1.png', 'img_16_5.png',
-     'img_8_0.png', 'img_10_2.png', 'img_12_0.png', 'img_12_4.png', 'img_10_6.png', 'img_16_0.png', 'img_14_2.png',
-     'img_14_3.png', 'img_8_5.png', 'img_16_1.png', 'img_14_1.png', 'img_14_0.png', 'img_16_2.png', 'img_10_4.png',
-     'img_12_6.png', 'img_2_0.png', 'img_0_2.png', 'img_18_0.png', 'img_6_4.png', 'img_4_6.png', 'img_18_1.png',
-     'img_0_3.png', 'img_2_1.png', 'img_2_3.png', 'img_18_2.png', 'img_0_0.png', 'img_2_2.png', 'img_0_4.png',
-     'img_6_2.png', 'img_4_0.png', 'img_4_1.png', 'img_6_3.png', 'img_0_5.png', 'img_2_5.png', 'img_6_1.png',
-     'img_18_5.png', 'img_4_3.png', 'img_4_2.png', 'img_18_4.png', 'img_6_0.png', 'img_5_5.png', 'img_1_1.png',
-     'img_3_3.png', 'img_3_2.png', 'img_1_0.png', 'img_7_6.png', 'img_19_2.png', 'img_5_4.png', 'img_19_0.png',
-     'img_7_4.png', 'img_1_2.png', 'img_3_0.png', 'img_3_1.png', 'img_1_3.png', 'img_7_5.png', 'img_19_1.png',
-     'img_5_3.png', 'img_19_5.png', 'img_3_4.png', 'img_1_6.png', 'img_19_4.png', 'img_7_0.png', 'img_5_2.png',
-     'img_5_0.png', 'img_7_2.png'], ['img_28_6.png', 'img_28_5.png', 'img_28_1.png', 'img_28_0.png', 'img_28_2.png',
-                                     'img_29_4.png', 'img_29_2.png', 'img_29_3.png', 'img_29_1.png', 'img_29_0.png',
-                                     'img_27_2.png', 'img_25_0.png', 'img_23_6.png', 'img_21_4.png', 'img_25_1.png',
-                                     'img_27_3.png', 'img_25_3.png', 'img_23_4.png', 'img_27_0.png', 'img_27_4.png',
-                                     'img_23_0.png', 'img_21_2.png', 'img_21_3.png', 'img_23_1.png', 'img_25_5.png',
-                                     'img_23_3.png', 'img_21_1.png', 'img_21_0.png', 'img_25_4.png', 'img_27_6.png',
-                                     'img_22_5.png', 'img_24_3.png', 'img_26_1.png', 'img_26_0.png', 'img_24_2.png',
-                                     'img_22_4.png', 'img_20_6.png', 'img_24_0.png', 'img_26_3.png', 'img_20_5.png',
-                                     'img_22_3.png', 'img_24_5.png', 'img_26_6.png', 'img_24_4.png', 'img_22_2.png',
-                                     'img_20_0.png', 'img_20_2.png', 'img_22_0.png', 'img_26_4.png', 'img_20_3.png'],
-    ['img_33_4.png', 'img_31_6.png', 'img_37_0.png', 'img_35_2.png', 'img_37_1.png', 'img_33_5.png', 'img_37_3.png',
-     'img_35_0.png', 'img_37_2.png', 'img_31_4.png', 'img_33_2.png', 'img_37_6.png', 'img_35_4.png', 'img_35_5.png',
-     'img_31_1.png', 'img_33_1.png', 'img_31_3.png', 'img_35_6.png', 'img_31_2.png', 'img_33_0.png', 'img_32_6.png',
-     'img_30_4.png', 'img_36_2.png', 'img_34_0.png', 'img_34_2.png', 'img_36_0.png', 'img_30_6.png', 'img_32_5.png',
-     'img_36_1.png', 'img_34_3.png', 'img_30_3.png', 'img_32_1.png', 'img_36_4.png', 'img_34_6.png', 'img_34_4.png',
-     'img_36_6.png', 'img_30_0.png', 'img_32_2.png', 'img_32_3.png', 'img_30_1.png', 'img_38_5.png', 'img_38_4.png',
-     'img_38_6.png', 'img_38_2.png', 'img_38_0.png', 'img_39_6.png', 'img_39_0.png', 'img_39_1.png', 'img_39_3.png',
-     'img_39_2.png'], ['img_40_2.png', 'img_41_3.png', 'img_42_1.png']
->>>>>>> 229e8b5 (final version 7Diff)
 
     ['img_37_6.png', 'img_34_3.png', 'img_30_1.png', 'img_33_2.png', 'img_30_0.png', 'img_35_4.png', 'img_32_5.png',
      'img_36_1.png', 'img_33_5.png', 'img_38_4.png', 'img_36_4.png', 'img_31_2.png', 'img_31_4.png', 'img_30_3.png',
@@ -66,5 +31,8 @@ images = [
      'img_38_5.png', 'img_36_0.png', 'img_39_0.png', 'img_37_3.png', 'img_31_1.png', 'img_31_6.png', 'img_36_2.png',
      'img_38_6.png', 'img_32_6.png', 'img_35_5.png', 'img_31_3.png', 'img_36_6.png', 'img_39_3.png', 'img_34_0.png',
      'img_33_1.png', 'img_35_2.png', 'img_34_2.png', 'img_35_0.png', 'img_39_6.png', 'img_39_2.png', 'img_39_1.png',
-     'img_38_0.png']
+     'img_38_0.png'],
+
+    ['img_40_2.png', 'img_41_3.png', 'img_42_1.png']
+
 ]
