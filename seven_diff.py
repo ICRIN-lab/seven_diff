2ef606bf6d8973e7ac795d7319f4f84b1e0cb13e
mselleki <mselleki98@gmail.com>
2022-04-28 14:10:36 +0200

    final version 7Diff

--- seven_diff.py
+++ seven_diff.py
@@ -8,7 +8,7 @@ from Template_Task_Psychopy.task_template import TaskTemplate
 class SevenDiff(TaskTemplate):
     # IMPORTANT ! To MODIFY IF NEEDED
     nb_ans = 4
-    response_pad = True  # has to be set on "True" on production.
+    response_pad = False  # has to be set on "True" on production.
     # END OF IMPORTANT
     left_key_name = "jaune"
     left_key_code = "0"
@@ -22,7 +22,9 @@ class SevenDiff(TaskTemplate):
     yes_key_code = "6"
     yes_key_name = "verte"
     keys = [left_key_code, mid_left_key_code, right_key_code, mid_right_key_code, yes_key_code, quit_code]
-    trials = 200  # SHOULD BE SET TO 200 WHEN JEANNE DO THE WORK
+    trials = 200  # number of trials
+    launch_example = True
+
     next = f"Pour passer à l'instruction suivante, appuyez sur la touche {yes_key_name}"
 
     # IMPORTANT : REWRITE INSTRUCTIONS
@@ -41,8 +43,10 @@ class SevenDiff(TaskTemplate):
             group = 0
         elif 100 <= no_trial < 150:
             group = 1
-        else:
+        elif 150 <= no_trial < 200:
             group = 2
+        else:
+            group = 3  # training images
         self.create_visual_image(image=f'img/{images[group][0]}',
                                  size=self.size(images[group][0])).draw()
         self.win.flip()
@@ -74,6 +78,39 @@ class SevenDiff(TaskTemplate):
                             round(rt, 2), round(time.time() - exp_start_timestamp, 2))
         images[group].pop(0)
         self.check_break(no_trial, 99, 149, test=False)  # put test=False on production
+        if practice:
+            return result
+
+    def example(self, exp_start_timestamp):
+        score_example = 0
+        example = self.create_visual_text(text="Commençons par un petit entraînement")
+        tutoriel_end = self.create_visual_text(
+            text="L'entraînement est désormais terminé"
+        )
+        example.draw()
+        self.create_visual_text(self.next, pos=(0, -0.4), font_size=0.04).draw()
+        self.win.flip()
+        self.wait_yes(self.response_pad)
+        for u in range(3):
+            if self.task(999, exp_start_timestamp, time.time(), True):
+                score_example += 1
+                self.create_visual_text(
+                    f"Bravo ! Vous avez {score_example}/{u+1}"
+                ).draw()
+                self.win.flip()
+                core.wait(2)
+            else:
+                self.create_visual_text(
+                    f"Dommage... Vous avez {score_example}/{u+1}"
+                ).draw()
+                self.win.flip()
+                core.wait(2)
+        self.create_visual_text(f"Vous avez obtenu {score_example}/3").draw()
+        self.win.flip()
+        core.wait(5)
+        tutoriel_end.draw()
+        self.win.flip()
+        core.wait(5)
 
 
 exp = SevenDiff(csv_folder="csv")
