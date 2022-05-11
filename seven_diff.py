6ba17158fa5320f8d30c07434a4f56e23c50b177
mselleki <mselleki98@gmail.com>
2022-05-11 20:14:43 +0200

    fix timing bug

--- seven_diff.py
+++ seven_diff.py
@@ -57,7 +57,7 @@ class SevenDiff(TaskTemplate):
         self.create_visual_text("0 \t\t /\t\t 1", pos=(-.6, -.4)).draw()
         self.create_visual_text("2 \t\t /\t\t 3+", pos=(.6, -.4)).draw()
         self.win.flip()
-        time_stamp = time.time() - self.exp_start_timestamp
+        time_stamp = time.time() - self.response_pad_timestamp
         resp, rt = self.get_response_with_time(self.response_pad)
         good_ans = self.get_good_ans(images[group][0][-5], {"0": self.left_key_code,
                                                             "1": self.mid_left_key_code,
